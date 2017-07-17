052d866bfb2d676af68db97212e8ffc0cf313d98
Alex Bumpers <apbumpers@gmail.com>
2017-07-16 21:27:46 -0500

    fix conflicts

--- wrapper/alerts/alerts.py
+++ wrapper/alerts/alerts.py
@@ -6,27 +6,20 @@ from keys import NPSSecretKey
 class NPS_Alerts_Write_Options(object):
     def __init__(self):
         """ Change this to TRUE if you need to write JSON data to file."""
-        self.WRITE_TO_FILE = True
+        self.WRITE_TO_FILE = False
 
 
 class NPS_Alerts(object):
     """
-    The NPS API does not seem to support seperating by category natively.
-    You can, however, parse this information via the JSON data output.
+    Query NPS API for *all* park alerts.
     """
-    def alerts_category(api_url):
-        payload = {
-        'category':'Danger', # From /Alerts/ GET category -> danger
-        'category':'caution', # From /Alerts/ GET category -> caution
-        'category':'information', # From /Alerts/ GET category -> information
-        'category':'park closure' # From /Alerts/ GET category -> park closure
-        }
+    def alerts(api_url):
         alert_url = requests.get(
             "https://developer.nps.gov/api/v0/alerts",
-            headers={'Authorization': NPSSecretKey.key}, params=payload)
+            headers={'Authorization': NPSSecretKey.key})
         write = NPS_Alerts_Write_Options()
         if write.WRITE_TO_FILE == True:
-            with open("alert_cats.json", "w") as outfile:
+            with open("alerts.json", "w") as outfile:
                 alert_dump = json.dump(
                 alert_url.json(),
                 outfile,
@@ -35,34 +28,4 @@ class NPS_Alerts(object):
                 ensure_ascii=False)
         alert_dump = alert_url.json()
         return alert_dump
-    alerts_category("https://developer.nps.gov/api/v0/alerts")
-
-    def alerts_description(api_url):
-        payload = {
-        'description':'true'
-        }
-        alert_url = requests.get(
-            "https://developer.nps.gov/api/v0/alerts",
-            headers={'Authorization': NPSSecretKey.key}, params=payload)
-        write = NPS_Alerts_Write_Options()
-        if write.WRITE_TO_FILE == True:
-            with open("alert_description.json", "w") as outfile:
-                alert_dump = json.dump(
-                alert_url.json(),
-                outfile,
-                sort_keys = True,
-                indent = 4,
-                ensure_ascii=False)
-        alert_dump = alert_url.json()
-        return alert_dump
-    alerts_description("https://developer.nps.gov/api/v0/alerts")
-
-
-
-
-
-# alert_url = requests.get(
-#     "https://developer.nps.gov/api/v0/alerts",
-#     headers={'Authorization': NPSSecretKey.key})
-# j = alert_url.json()
-# print(j['category'][1]['danger'])
+    alerts("https://developer.nps.gov/api/v0/alerts")
