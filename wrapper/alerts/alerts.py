0b42fc0249f9c97b6ed55aa211824a30c78b9a69
Alex Bumpers <apbumpers@gmail.com>
2017-07-16 21:20:05 -0500

    Remove params, query all alerts.

--- wrapper/alerts/alerts.py
+++ wrapper/alerts/alerts.py
@@ -11,22 +11,15 @@ class NPS_Alerts_Write_Options(object):
 
 class NPS_Alerts(object):
     """
-    The NPS API does not seem to support seperating by category natively.
-    You can, however, parse this information via the JSON data output.
+    Query NPS API for *all* park alerts.
     """
-    def alerts_category(api_url):
-        payload = {
-        'category':'Danger', # From /Alerts/ GET category -> danger
-        'category':'caution', # From /Alerts/ GET category -> caution
-        'category':'information', # From /Alerts/ GET category -> information
-        'category':'park closure' # From /Alerts/ GET category -> park closure
-        }
+    def alerts(api_url):
         alert_url = requests.get(
             "https://developer.nps.gov/api/v0/alerts",
-            headers={'Authorization': NPSSecretKey.key}, params=payload)
+            headers={'Authorization': NPSSecretKey.key})
         write = NPS_Alerts_Write_Options()
         if write.WRITE_TO_FILE == True:
-            with open("alert.txt", "w") as outfile:
+            with open("alerts.json", "w") as outfile:
                 alert_dump = json.dump(
                 alert_url.json(),
                 outfile,
@@ -35,4 +28,4 @@ class NPS_Alerts(object):
                 ensure_ascii=False)
         alert_dump = alert_url.json()
         return alert_dump
-    alerts_category_danger("https://developer.nps.gov/api/v0/alerts")
+    alerts("https://developer.nps.gov/api/v0/alerts")
